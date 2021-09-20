30e462a2287c6180b6f79a8f5f1570c5160641f9
philemmons <pemmons@csumb.edu>
2021-09-19 23:57:37 -0700

    added style divs to index.php

--- index.php
+++ index.php
@@ -11,25 +11,30 @@
   <![endif]-->
 </head>
 <body>
-    <div class= 'header'><h2>Survey 2021</h2></div>
+  <div class='wrapOverall'>
+    <div class= 'header'><h2>Survey 2021</h2></div><!--end header -->
+      <div class='wrapContent'>
 
-    <div class='sideNav'>
-      <ul>
-        <li><a href="#">Survey 1</a></li>
-        <li><a href="#">Survey 1</a></li>
-      </ul>
-    </div>
-    <div class= 'content'>
-      <h2>Survey Name</h2>
-      <h3>Survey Name</h3>
-      <form action="index.php" method="post">
-        <input type="text" name="answer" size="30" />
-        <input type="submit" value="Submit" name="submit" />
-        <input type="hidden" value="questionid" value="questionid" />
-        <input type="hidden" value="submitted" value="1" />
-      </form>
-    </div>
+          <div class='sideNav'>
+            <ul>
+              <li><a href="#">Survey 1</a></li>
+              <li><a href="#">Survey 1</a></li>
+            </ul>
+          </div>
 
+          <div class= 'content'>
+            <h2>Survey Name</h2>
+            <h3>Survey Name</h3>
+            <form action="index.php" method="post">
+                <input type="text" name="answer" size="30" />
+                <input type="submit" value="Submit" name="submit" />
+                <input type="hidden" value="questionid" value="questionid" />
+                <input type="hidden" value="submitted" value="1" />
+            </form>
+          </div><!--end content -->
+      </div><!--end wrapContent -->
+<div class= 'footer'><p><a href=admin/index.php>Admin Panel</a></p></div><!--end footer -->
+</div><!--end wrapOverall -->
 
   <script src="js/scripts.js"></script>
 </body>
