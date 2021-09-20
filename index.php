7283e663bbe943bd52a88dcc3e469ef03e9ba126
philemmons <pemmons@csumb.edu>
2021-09-19 23:44:24 -0700

    added survey html form tag to index.php

--- index.php
+++ index.php
@@ -11,6 +11,26 @@
   <![endif]-->
 </head>
 <body>
+    <div class= 'header'><h2>Survey 2021</h2></div>
+
+    <div class='sideNav'>
+      <ul>
+        <li><a href="#">Survey 1</a></li>
+        <li><a href="#">Survey 1</a></li>
+      </ul>
+    </div>
+    <div class= 'content'>
+      <h2>Survey Name</h2>
+      <h3>Survey Name</h3>
+      <form action="index.php" method="post">
+        <input type="text" name="answer" size="30" />
+        <input type="submit" value="Submit" name="submit" />
+        <input type="hidden" value="questionid" value="questionid" />
+        <input type="hidden" value="submitted" value="1" />
+      </form>
+    </div>
+
+
   <script src="js/scripts.js"></script>
 </body>
 </html>
